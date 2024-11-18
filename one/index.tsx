67a087c158c308fc75187ac393640883ccb6ac56
MARKjotep <markjotep@gmail.com>
2024-11-18 18:24:51 +0800

    new

---
+++ one/index.tsx
@@ -0,0 +1,5 @@
+import { $$, frag, dom } from "hellmo";
+
+export function ONE() {
+  return <main>main ui huhu</main>;
+}
---
+++ ui/index.tsx
@@ -0,0 +1,5 @@
+import { $$, frag, dom } from "hellmo";
+
+export function UI() {
+  return <main>main ui wow</main>;
+}
